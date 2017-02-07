40809525a26b4583787dc68363d1574f024419b8
Azalee Bostroem <abostroem@gmail.com>
2017-02-06 21:06:20 -0800

    moved calculation of H into integral and fixed sign error on rho_k Not in this commit, but in a5fcf470fb0931df258c148fc37cefc988429a78 added 1/a to luminosity distance calculation

--- hw4/calculate_chi.m
+++ hw4/calculate_chi.m
@@ -1,12 +1,8 @@
 function chi = calculate_chi(a, today)
     %calculate chi for luminosity distance at a  
-    constants = load_constants();
-    rho = calc_energy_density(a, today);
-    
-    H = sqrt(8.*pi*constants.G/3. .* (rho.m+rho.r+rho.L - rho.k));
-    norm_H = H ./ today.H;
-    chi = linspace(-999, -999, length(a));
+
+    chi = zeros(1, length(a));
     for n=1:length(a)
-        chi(n) = quad(@(int_a) chi_integrand(int_a,norm_H(n)),a(n),today.a);
+        chi(n) = quad(@(int_a) chi_integrand(int_a,today),a(n),today.a);
     end
     chi = chi ./ today.H;
