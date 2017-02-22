29956a41799441a49681e2fa91c2e9457b4c08ad
Azalee Bostroem <abostroem@gmail.com>
2017-02-22 15:20:55 -0800

    missed a few files that were updated to point to evolve_phi.m

--- hw5/problem5_4b.m
+++ hw5/problem5_4b.m
@@ -1,27 +1,21 @@
-params = load_fixed_params();
 a_initial = 1.;
 phi_initial = 2.0;
-phidot_initial = find_phidot(phi_initial, params);
-phidot_initial
-
-rho_m0 = find_rho_m0(a_initial, phi_initial, phidot_initial, params)
-params.rho_m0 = rho_m0;
-
-phidot_initial = 0.22;
-input_array_start = [phi_initial; a_initial; phidot_initial];
+phidot_in = 0.22;
 tspan = logspace(-10, 3, 100);
+a0 = 'default';
+rho_m0_in = 'default';
+npts_return = 100;
 
-
-[t phi solution] = find_phi(input_array_start, tspan, params);
+[t phi phidot V a] = evolve_phi(a_initial, phi_initial, phidot_in, a0, rho_m0_in, tspan, npts_return);
 
 phi_5_3a = 2./params.lambda * log(params.lambda/2 * (2.*sqrt(params.V_0)*t));
-f=figure()
-plot(t, phi, 'b')
+f=figure();
+plot(t, phi, 'b');
 hold on
-plot(t, phi_5_3a+2.3, 'r--')
+plot(t, phi_5_3a+2.3, 'r--');
 l = legend('Numerical phi (5.4b)', 'Analytic phi (5.3a)');
 l.Location='Best';
-title(strcat('\phi for \phi << \beta ;  \phi_{start}=', num2str(phi_initial), ';  \phidot_{start} = ', num2str(phidot_initial)));
+title(strcat('\phi for \phi << \beta ;  \phi_{start}=', num2str(phi_initial), ';  \phidot_{start} = ', num2str(phidot_in)));
 xlabel('Time');
 ylabel('\phi');
 saveas(f, 'comp_numerical_analytical.pdf');
--- hw5/problem5_4d.m
+++ hw5/problem5_4d.m
@@ -1,28 +1,18 @@
-params = load_fixed_params();
-params.V_0 = 1.0E120
 a_initial = 1.;
-phi_initial = 33.8
-phidot_initial = find_phidot(phi_initial, params);
-phidot_initial
-rho_m0 = find_rho_m0(a_initial, phi_initial, phidot_initial, params)
-params.rho_m0 = rho_m0;
+phi_initial = 33.9999;
+phidot_in = 'default';
+rho_m0_in = 'default';
+a0 = 'default';
+tspan = [0 100];
+npts_return = 10;
 
-phidot_initial = 10;
-input_array_start = [phi_initial; a_initial; phidot_initial];
-tspan = [0 6];
-
-
-[t phi solution] = find_phi(input_array_start, tspan, params);
-
-phidot = solution.y(3, :);
-
-t = linspace(0, 6, 10);
-y = deval(solution, t);
-phi = y(1, :);
-phidot = y(3, :);
-V = calc_v(phi, params);
+[t phi phidot V a] = evolve_phi(a_initial, phi_initial, phidot_in, a0, rho_m0_in, tspan, npts_return);
 
 energy_density = 0.5 .* phidot .^2 + V;
 
 %From HW2 in reduced plank units
-rho_0c = 3 * H_0^2
+H_0 = 2.22E-52;
+rho_0c = 3 * H_0^2; %8piG = 1 in problem 5 units
+energy_density_hw2 = 0.72 * rho_0c;
+
+sprintf('dark energy density = %e from this code, = %e from HW2', energy_density(5), energy_density_hw2) 
