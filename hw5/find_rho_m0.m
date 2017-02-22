9b18774cd8649a11f7072f6b66e734f010c8c46a
Azalee Bostroem <abostroem@gmail.com>
2017-02-22 10:23:05 -0800

    cleaning up coding

--- hw5/find_rho_m0.m
+++ hw5/find_rho_m0.m
@@ -1,6 +1,7 @@
 function rho_m0 = find_rho_m0(a, phi, phidot, params)
     V = calc_v(phi, params);
-    A = params.lambda.^2./3. - 1.;
-    rho_m = A .* (phidot.^2/2. + V);
+    A = params.lambda.^2./3. - 1;
+    rho_phi = (0.5 .* phidot.^2 + V)
+    rho_m = A .* rho_phi; %use scaling relation
     rho_m0 = rho_m .* (a./params.a0).^3;
 end
