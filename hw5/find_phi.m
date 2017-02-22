1e8bfb3af64ee623aaf88e28435345087a431461
Azalee Bostroem <abostroem@gmail.com>
2017-02-22 15:20:16 -0800

    adding semicolons

--- hw5/find_phi.m
+++ hw5/find_phi.m
@@ -1,5 +1,5 @@
 function [t phi solution] = find_phi(initial_values, tspan, params)
-    solution = ode45(@(t, input_array) EOM_def(t, input_array, params), tspan, initial_values)
+    solution = ode45(@(t, input_array) EOM_def(t, input_array, params), tspan, initial_values);
     t = solution.x;
     phi = solution.y(1, :);
     a = solution.y(2, :);
--- hw5/find_rho_m0.m
+++ hw5/find_rho_m0.m
@@ -1,7 +1,7 @@
 function rho_m0 = find_rho_m0(a, phi, phidot, params)
     V = calc_v(phi, params);
     A = params.lambda.^2./3. - 1;
-    rho_phi = (0.5 .* phidot.^2 + V)
+    rho_phi = (0.5 .* phidot.^2 + V);
     rho_m = A .* rho_phi; %use scaling relation
     rho_m0 = rho_m .* (a./params.a0).^3;
 end
